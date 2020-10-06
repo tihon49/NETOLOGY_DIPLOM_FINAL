1f86de19d5ea8d29d2b3e6ab698757771ed475b1
Tihon49 <tihon49@gmail.com>
2020-10-06 13:55:54 +0300

    ShopUpdateView added

--- api/urls.py
+++ api/urls.py
@@ -1,10 +1,10 @@
 from django.urls import path, include
 
-from shop.views import ShopsListView, ShopDetailView, ShopCreateView
+from shop.views import ShopsListView, ShopDetailView, ShopCreateView, ShopUpdateView
 
 urlpatterns = [
     path('shop/', ShopDetailView.as_view(), name='shop_detail'),
-    # path('shop/<int:pk>/update/', ShopUpdateView.as_view(), name='shop_update'),
+    path('shop/update/<int:pk>', ShopUpdateView.as_view(), name='shop_update'),
     path('shop/list/', ShopsListView.as_view(), name='shops_list_view'),
     path('shop/create', ShopCreateView.as_view(), name='shop_create'),
 ]
--- shop/views.py
+++ shop/views.py
@@ -2,9 +2,9 @@ from rest_framework import generics
 from rest_framework.response import Response
 from rest_framework.views import APIView
 
-from api.permissions import IsOwnerOrReadOnly
+from api.permissions import IsOwnerOrReadOnly, IsShopOwnerOrReadOnly
 from shop.models import Shop
-from shop.serializers import ShopsListSerializer, ShopDetailSerializer
+from shop.serializers import ShopsListSerializer, ShopDetailSerializer, ShopCreteSerializer
 from rest_framework.permissions import IsAdminUser, IsAuthenticated
 
 
@@ -25,6 +25,13 @@ class ShopDetailView(APIView):
         shop = Shop.objects.get(user = user)
         serializer = ShopDetailSerializer(shop)
         return Response(serializer.data)
+
+
+#https://www.youtube.com/watch?v=C6S3dMt1s_M&t=5074s   at 1:24:55
+class ShopUpdateView(generics.RetrieveUpdateDestroyAPIView):
+    serializer_class = ShopDetailSerializer
+    queryset = Shop.objects.all()
+    permission_classes = (IsShopOwnerOrReadOnly,)
 
 
 # class ShopDetailView(generics.RetrieveUpdateDestroyAPIView):
